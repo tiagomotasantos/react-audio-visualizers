11b4bd7d1bf05da5471c4cbc07a86cbc43665622
Tiago Mota Santos <tiago.mota@percona.com>
2021-06-27 19:05:55 +0100

    Refactor formatting

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ function App() {
   return (
     <div>
       <AudioVisualizer audio="https://media1.vocaroo.com/mp3/111ewAKiEfXZ">
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={'hotpink'} />
-      </mesh>
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={'hotpink'} />
+        </mesh>
       </AudioVisualizer>
     </div>
   );
