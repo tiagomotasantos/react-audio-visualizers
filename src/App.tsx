a07844c42ce5402a29edc01d00197598cfd09d90
Tiago Mota Santos <tiago.mota@percona.com>
2021-07-05 21:44:24 +0100

    Use SpectrumVisualizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { AudioVisualizer, useAudioVisualizerContext } from 'packages/react-audio-visualizers-core/src';
+import { SpectrumVisualizer, SpectrumVisualizerTheme } from 'SpectrumVisualizer';
 
-const Box = () => {
-  const { analyser } = useAudioVisualizerContext();
+// const Box = () => {
+//   const { analyser } = useAudioVisualizerContext();
 
-  console.log('audio context:', analyser);
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'hotpink'} />
-    </mesh>
-  );
-};
-
+//   console.log('audio context:', analyser);
+//   return (
+//     <mesh>
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={'hotpink'} />
+//     </mesh>
+//   );
+// };
 
 function App() {
   return (
     <div>
-      <AudioVisualizer audio="https://media1.vocaroo.com/mp3/111ewAKiEfXZ">
-        <Box/>
-      </AudioVisualizer>
+      <SpectrumVisualizer
+        audio="https://media1.vocaroo.com/mp3/111ewAKiEfXZ"
+        theme={SpectrumVisualizerTheme.roundBars}
+      />
     </div>
   );
 }
