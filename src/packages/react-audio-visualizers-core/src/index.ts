1a97d0220746b85fe3d6102a917d0c5e0a3a2b6f
Tiago Mota Santos <tiago.mota@percona.com>
2021-07-11 19:06:56 +0100

    Add color type to core

--- src/packages/react-audio-visualizers-core/src/index.ts
+++ src/packages/react-audio-visualizers-core/src/index.ts
@@ -1,1 +1,2 @@
 export * from './AudioVisualizer';
+export * from './types';
---
+++ src/packages/react-audio-visualizers-core/src/types.ts
@@ -0,0 +1,3 @@
+export type ColorGradient = { color1: string, color2: string };
+
+export type Color = THREE.Color | ColorGradient | string | number;
