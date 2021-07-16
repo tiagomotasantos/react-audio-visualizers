4ee986c5d671ae7b88f03dd9bb48f66b62f1e84b
Tiago Mota Santos <tiago.mota@percona.com>
2021-07-16 12:04:37 +0100

    Change icons size

--- src/packages/react-audio-visualizers-core/src/AudioVisualizer/icons/PauseIcon.tsx
+++ src/packages/react-audio-visualizers-core/src/AudioVisualizer/icons/PauseIcon.tsx
@@ -1,5 +1,5 @@
 import { SVGProps } from 'react';
 
 export const PauseIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.6 47.6" {...props}><path d="M18 41c0 3.7-3 6.6-6.6 6.6l0 0c-3.7 0-6.6-3-6.6-6.6V6.6C4.7 3 7.7 0 11.4 0l0 0c3.7 0 6.6 3 6.6 6.6V41z" /><path d="M42.9 41c0 3.7-3 6.6-6.6 6.6l0 0c-3.7 0-6.6-3-6.6-6.6V6.6C29.6 3 32.6 0 36.2 0l0 0c3.7 0 6.6 3 6.6 6.6V41z" /></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 47.6 47.6" {...props}><path d="M18 41c0 3.7-3 6.6-6.6 6.6l0 0c-3.7 0-6.6-3-6.6-6.6V6.6C4.7 3 7.7 0 11.4 0l0 0c3.7 0 6.6 3 6.6 6.6V41z" /><path d="M42.9 41c0 3.7-3 6.6-6.6 6.6l0 0c-3.7 0-6.6-3-6.6-6.6V6.6C29.6 3 32.6 0 36.2 0l0 0c3.7 0 6.6 3 6.6 6.6V41z" /></svg>
 );
--- src/packages/react-audio-visualizers-core/src/AudioVisualizer/icons/PlayIcon.tsx
+++ src/packages/react-audio-visualizers-core/src/AudioVisualizer/icons/PlayIcon.tsx
@@ -1,5 +1,5 @@
 import { SVGProps } from 'react';
 
 export const PlayIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="164" height="164" viewBox="0 0 163.9 163.9" {...props}><path d="M34.9 3.6C20.1-4.9 8.1 2.1 8.1 19.1v125.6c0 17 12 24 26.8 15.5L144.7 97.3c14.8-8.5 14.8-22.2 0-30.7L34.9 3.6z"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 163.9 163.9" {...props}><path d="M34.9 3.6C20.1-4.9 8.1 2.1 8.1 19.1v125.6c0 17 12 24 26.8 15.5L144.7 97.3c14.8-8.5 14.8-22.2 0-30.7L34.9 3.6z"/></svg>
 );
