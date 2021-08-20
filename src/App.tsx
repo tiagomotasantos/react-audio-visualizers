c3d91818b279bac46c7cf9f3a50242e0edc2c248
Tiago Mota Santos <tiago.mota@percona.com>
2021-08-20 23:22:16 +0100

    Use WaveformVisualizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { SpectrumVisualizer, SpectrumVisualizerTheme } from 'SpectrumVisualizer';
+import { WaveformVisualizer, WaveformVisualizerTheme } from 'WaveformVisualizer';
 
 // const Box = () => {
 //   const { analyser } = useAudioVisualizerContext();
@@ -15,10 +16,20 @@ import { SpectrumVisualizer, SpectrumVisualizerTheme } from 'SpectrumVisualizer'
 function App() {
   return (
     <div style={{ height: '100%' }}>
-      <SpectrumVisualizer
+      {/* <SpectrumVisualizer
         audio="https://media1.vocaroo.com/mp3/13HVOqUk0kGc"
         volume={1}
         theme={SpectrumVisualizerTheme.radialSquaredBars}
+        colors={['#ff9670', '#b2694e']}
+        iconsColor="#ff9670"
+        backgroundColor="black"
+        showMainActionIcon
+        showLoaderIcon
+      /> */}
+      <WaveformVisualizer
+        audio="https://media1.vocaroo.com/mp3/13HVOqUk0kGc"
+        volume={1}
+        theme={WaveformVisualizerTheme.squaredBars}
         colors={['#ff9670', '#b2694e']}
         iconsColor="#ff9670"
         backgroundColor="black"
